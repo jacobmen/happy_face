1a690f6d5f93a0df45b3b3e85ad9d00d1d2611d9
Jacob Men <jacobmen416@yahoo.com>
2021-03-08 15:09:26 -0600

    basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod types;
+mod server;
 
 use types::Message;
 
 fn main() {
     let mes = Message::new("jacob", "bob", "hello world");
     println!("{:?}", mes);
-    let msg_str = serde_json::to_string(&mes).unwrap();
-    println!("{}", msg_str);
+    let msg_str = bincode::serialize(&mes).unwrap();
+    println!("{:?}", msg_str);
 }
