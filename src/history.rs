0d221b706dd6c9d33dc5ec005be9aaa273596da7
Amy Qian <amyyq2@illinois.edu>
2021-04-09 15:42:06 -0500

    Tested history and added error checking

--- src/history.rs
+++ src/history.rs
@@ -28,24 +28,28 @@ pub fn insert_message(key: &str, msg: Message) -> Result<()> {
 pub fn get_history(key: &str) -> String {
   let db = sled::open("history_db");
 
-  let value = db.unwrap().get(key).unwrap().unwrap();
-  let history_vector: Vec<Message> = bincode::deserialize(&value).unwrap();
+  if let Some(value) = db.unwrap().get(key).unwrap() {
+    let history_vector: Vec<Message> = bincode::deserialize(&value).unwrap();
   
-  // only send 10 most recent Messages
-  let mut start_index = 0;
-  if history_vector.len() > 10{
-    start_index = history_vector.len() - 10;
-  }
+    // only send 10 most recent Messages
+    let mut start_index = 0;
+    if history_vector.len() > 10{
+      start_index = history_vector.len() - 10;
+    }
 
-  let mut history_string = "".to_string();
-  for x in start_index..history_vector.len() {
-    history_string.push_str(history_vector[x].sender);
-    history_string.push_str(": ");
-    history_string.push_str(history_vector[x].content);
-    history_string.push_str("\n");
-  }
+    let mut history_string = "".to_string();
+    for x in start_index..history_vector.len() {
+      history_string.push_str(history_vector[x].sender);
+      history_string.push_str(": ");
+      history_string.push_str(history_vector[x].content);
+      history_string.push_str("\n");
+    }
 
-  return history_string;
+    return history_string;
+  } else {
+    return "No history yet!".to_string();
+  }
+  
 }
 
 pub fn clear_history() -> Result<()> {
